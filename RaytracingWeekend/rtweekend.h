3a782c2718c6a434461bcaf26e47f4a088beb7e9
Philipp Jurasic <philipp.jurasic@gmx.de>
2022-07-02 00:28:23 +0200

    refactor and changes to random generation

--- RaytracingWeekend/rtweekend.h
+++ RaytracingWeekend/rtweekend.h
@@ -8,7 +8,8 @@
 
 
 //#define DEBUG_DEPTH
-#define DISPERSION
+//#define DISPERSION
+//#define DISCRETE_DISPERSION
 
 // Usings
 using std::shared_ptr;
@@ -16,7 +17,8 @@ using std::make_shared;
 using std::sqrt;
 
 // Constants
-const double infinity = std::numeric_limits<double>::infinity();
+const double global_t_min = 1e-6; //DBL_EPSILON?
+const double infinity = DBL_MAX;
 const double pi = 3.1415926535897932385;
 const double aspect_ratio = 16.0 / 9.0;
 const double white_wavelength = 550.;
@@ -36,13 +38,14 @@ inline double clamp(const double x, const double min=0, const double max=1) {
 
 //pcg random
 static pcg32_fast pcg_generator;
-const auto precompute_num = 65729;
+const int precompute_num = 16777216; //16mio random numbers should be enough... right?
 double random_values[precompute_num];
-int random_index = 0;
+std::atomic_uint random_index = 0; //This is a uint so it overflows to 0 instead of a negative number. Otherwise more expensive checks would be needed
 
 //book random
-static std::mt19937 generator;
+static std::mt19937 twister_generator;
 static std::uniform_real_distribution<double> distribution;
+static std::normal_distribution<double> normal_distribution{0., .2}; //normal distribution approximately between -.5 and .5
 
 void init_random() {
     pcg_extras::seed_seq_from<std::random_device> seed_source;
@@ -52,11 +55,16 @@ void init_random() {
     }
 }
 
+inline double random_normal_double() {
+    return normal_distribution(twister_generator);
+}
+
 inline double random_double() {
     /*
     return distribution(pcg_generator); //84s
-    return distribution(generator); //67s vs original 70s when creating a new distribution every frame
+    return distribution(twister_generator); //67s vs original 70s when creating a new distribution every frame
     return random_values[rng(precompute_num)]; //75s
+    return random_values[random_index++ % precompute_num]; //29s
     */
     return random_values[random_index++ % precompute_num]; //29s
 }
