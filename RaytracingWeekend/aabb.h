2c1d96a480ccd1d34cb95055dd55c06797cb883d
Philipp Jurasic <philipp.jurasic@gmx.de>
2023-02-19 21:21:26 +0100

    faster aabb s

--- RaytracingWeekend/aabb.h
+++ RaytracingWeekend/aabb.h
@@ -3,14 +3,14 @@
 
 class aabb{
 public:
-	aabb(){}
+    aabb() = default;
 	aabb(const point3& a, const point3& b): minimum(a),maximum(b) {}
 
 	point3 min() const { return minimum; }
 	point3 max() const { return maximum; }
 
     inline bool hit(const ray& r, double t_min, double t_max) const {
-        const vec3 orig = r.origin(); //this seems to speed up the calculation by 1-2% due to better memory locality
+        /*const vec3 orig = r.origin();
         for (int a = 0; a < 3; a++) {
             const auto invD = 1.0 / r.direction()[a];
             auto t0 = (min()[a] - orig[a]) * invD;
@@ -23,8 +23,22 @@ public:
                 return false;
         }
         return true;
+        */
+
+        // Slightly More performant hit test 
+		const auto invDir = 1./r.direction();
+		auto t0 = (min() - r.origin())*invDir;
+		auto t1 = (max() - r.origin())*invDir;
+	
+        const auto t_min_aabb = glm::min(t0,t1);
+        const auto t_max_aabb = glm::max(t0,t1);
+
+        t_min = std::max({ t_min, t_min_aabb[0], t_min_aabb[1], t_min_aabb[2] });
+        t_max = std::min({ t_max, t_max_aabb[0], t_max_aabb[1], t_max_aabb[2] });
+
+		return (t_min < t_max);
     }
-public:
+private:
 	point3 minimum;
 	point3 maximum;
 };
